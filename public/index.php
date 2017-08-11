f834b2ac7bbafcee197a831943144a293be93694
David Ludwig <davidludwigii@gmail.com>
2017-08-11 00:31:53 -0500

    Adsense implementation

--- public/index.php
+++ public/index.php
@@ -8,5 +8,6 @@ $images = GalleryImage::all();
 
 view('home', [
 	'title' => "InterCraft - Minecraft Survival Server",
-	'images' => array_slice($images, 0, 6)
+	'images' => array_slice($images, 0, 6),
+	'adsenseId' => config()['adsense']['google_ad_client']
 ]);
--- resources/views/home.php
+++ resources/views/home.php
@@ -5,6 +5,13 @@
 	<?php component("metadata"); ?>
 	<link rel="stylesheet" href="assets/css/lightgallery.css">
 	<link rel="stylesheet" href="assets/css/lg-transitions.min.css">
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<script>
+		(adsbygoogle = window.adsbygoogle || []).push({
+			google_ad_client: "<?php echo $adsenseId; ?>",
+			enable_page_level_ads: true
+		});
+	</script>
 </head>
 <body>
 	<div class="page">
