f701e23a01af501f9f2ec31dce1801b7b6a0edc6
David Ludwig <davidludwigii@gmail.com>
2017-09-24 15:30:01 -0500

    Added metadeta

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="Lightly Modded Minecraft Survival Server">
+    <meta name="keywords" content="Minecraft,Modded,Vanilla,Whitelisted,OpenComputers">
+    <meta name="author" content="David Ludwig">
     <meta name="theme-color" content="#333">
     <meta name="msapplication-navbutton-color" content="#333">
     <meta name="apple-mobile-web-app-status-bar-style" content="#222">
